806d4d174176654ed6f87e7f210167961f514f4b
bulesub <107999712+bulesub@users.noreply.github.com>
2025-03-18 18:45:03 +0900

    우측 이미지에 정보 전달

--- GSolution/GProject/GProjectDlg.cpp
+++ GSolution/GProject/GProjectDlg.cpp
@@ -7,6 +7,7 @@
 #include "GProject.h"
 #include "GProjectDlg.h"
 #include "afxdialogex.h"
+#include <iostream>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -16,6 +17,7 @@
 
 
 // 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+using namespace std;
 
 class CAboutDlg : public CDialogEx
 {
@@ -45,6 +47,7 @@ void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
 
@@ -65,10 +68,11 @@ void CGProjectDlg::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CGProjectDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
+//	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
-	ON_BN_CLICKED(IDC_BTN_DLG, &CGProjectDlg::OnBnClickedBtnDlg)
 	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_TEST, &CGProjectDlg::OnBnClickedBtnTest)
+	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
 
@@ -104,11 +108,17 @@ BOOL CGProjectDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	MoveWindow(0, 0, 1280, 800);
 	m_pDlgImage = new CDlgImage;
 	// Create시에 this 넣는 이유
 	// -> 자식 Dialog에서 부모 Dialog를 참조할 수 있게(모달리스가 가능하도록)
 	m_pDlgImage->Create(IDD_CDlgImage, this);
 	m_pDlgImage->ShowWindow(SW_SHOW);
+	m_pDlgImage->MoveWindow(0, 0, 640, 480);
+	m_pDlgImageResult = new CDlgImage;
+	m_pDlgImageResult->Create(IDD_CDlgImage, this);
+	m_pDlgImageResult->ShowWindow(SW_SHOW);
+	m_pDlgImageResult->MoveWindow(640, 0, 640, 480);
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -130,31 +140,6 @@ void CGProjectDlg::OnSysCommand(UINT nID, LPARAM lParam)
 //  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
 //  프레임워크에서 이 작업을 자동으로 수행합니다.
 
-void CGProjectDlg::OnPaint()
-{
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-
-		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
-
-		// 아이콘을 그립니다.
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialogEx::OnPaint();
-	}
-}
-
 // 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
 //  이 함수를 호출합니다.
 HCURSOR CGProjectDlg::OnQueryDragIcon()
@@ -162,21 +147,54 @@ HCURSOR CGProjectDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-void CGProjectDlg::OnBnClickedBtnDlg()
-{
-	m_pDlgImage->ShowWindow(SW_SHOW);
-}
-
 void CGProjectDlg::OnDestroy()
 {
 	CDialogEx::OnDestroy();
 
-	delete m_pDlgImage;
+	if (m_pDlgImage)		delete m_pDlgImage;
+	if (m_pDlgImageResult)	delete m_pDlgImageResult;
 }
 
-#include <iostream>
 void CGProjectDlg::CallFunc(int n)
 {
 	//int nData = n;
 	std::cout << n << std::endl;
+}
+
+void CGProjectDlg::OnBnClickedBtnTest()
+{
+	unsigned char* fm = (unsigned char*)m_pDlgImage->m_image.GetBits();
+	int nWidth = m_pDlgImage->m_image.GetWidth();
+	int nHeight = m_pDlgImage->m_image.GetHeight();
+	int nPitch = m_pDlgImage->m_image.GetPitch();
+	memset(fm, 0xff, nWidth * nHeight);
+
+	for (int k = 0; k < 100; k++)
+	{
+		int x = rand() % nWidth;
+		int y = rand() % nHeight;
+		fm[y * nPitch + x] = 0;
+	}
+
+	//memset(fm, 0, 320 * 240);
+
+	int nIndex = 0;
+	for (int j = 0; j < nHeight; j++)
+	{
+		for (int i = 0; i < nWidth; i++)
+		{
+			if (fm[j * nPitch + i] == 0)
+			{
+				if (m_pDlgImageResult->m_nDataCount < 100)
+				{
+					m_pDlgImageResult->m_pointData[nIndex].x = i;
+					m_pDlgImageResult->m_pointData[nIndex].y = j;
+					m_pDlgImageResult->m_nDataCount = ++nIndex;
+				}
+			}
+		}
+	}
+
+	m_pDlgImage->Invalidate();
+	m_pDlgImageResult->Invalidate();
 }
